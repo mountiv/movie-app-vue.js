6701a48579c62902f8523ce20e2f1f6a0199be08
mountiv <121834775+mountiv@users.noreply.github.com>
2022-10-08 19:26:08 +0600

    card done

--- src/components/MoviesListTable.vue
+++ src/components/MoviesListTable.vue
@@ -10,7 +10,10 @@
       <div v-for="(movie, key, id) in list" :key="key">
         <BRow class="text-center movie-table-row">
           <BCol class="text-start" cols="6">
-            <BButton v-b-toggle="`${id}`" variant="primary"
+            <BButton
+              v-b-toggle="`${id}`"
+              class="text-white text-start"
+              variant="transparent"
               >{{ id + movieId + "." }} {{ movie.Title }} ({{
                 movie.Year
               }})</BButton
@@ -24,22 +27,55 @@
             {{ movie.Ratings[2] ? movie.Ratings[2].Value : "-" }}
           </BCol>
           <BCollapse cols="12" :id="`${id}`" class="mt-2">
-            <BCard class="text-dark">
+            <BCard class="text-light movie-card">
               <BRow no-gutters>
-                <BCol md="6">
+                <BCol md="3">
                   <BCard-img
                     :src="movie.Poster"
                     alt="Image"
                     class="rounded-0"
                   ></BCard-img>
                 </BCol>
-                <BCol md="6">
-                  <BCard-body :title="movie.Title">
-                    <BCard-text>
-                      This is a wider card with supporting text as a natural
-                      lead-in to additional content. This content is a little
-                      bit longer.
-                    </BCard-text>
+                <BCol md="9">
+                  <BCard-body>
+                    <div class="movie-info">
+                      <BCard-text>
+                        <b class="movie-info-header">Award:</b>
+                        {{ movie.Awards }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Country:</b>
+                        {{ movie.Country }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Genre:</b>
+                        {{ movie.Genre }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Runtime:</b>
+                        {{ movie.Runtime }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Released:</b>
+                        {{ movie.Released }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Director:</b>
+                        {{ movie.Director }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Actors:</b>
+                        {{ movie.Actors }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Writer:</b>
+                        {{ movie.Writer }}
+                      </BCard-text>
+                      <BCard-text>
+                        <b class="movie-info-header">Discription:</b>
+                        {{ movie.Plot }}
+                      </BCard-text>
+                    </div>
                   </BCard-body>
                 </BCol>
               </BRow>
@@ -99,6 +135,27 @@ export default {
 }
 .movies-table-header {
   margin-bottom: 10px;
+}
+.movie-card {
+  background-color: transparent;
+}
+.movie-info {
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  font-size: 14px;
+}
+.card-body {
+  padding-top: 0 !important;
+}
+.movie-info-header {
+  font-size: 12px;
+}
+p {
+  margin-bottom: 5px;
+}
+.btn:focus {
+  box-shadow: none !important;
 }
 @media (max-width: 768px) {
   .movies-table {
